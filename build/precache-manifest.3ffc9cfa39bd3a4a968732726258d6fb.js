25a70c3796c338304848d44aca4d54aa61bd1424
KITHU <james.kithu2@gmail.com>
2019-10-18 08:39:36 +0300

    add logic to deploy to firebase

---
+++ build/precache-manifest.3ffc9cfa39bd3a4a968732726258d6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526249201884f90ed9a6d619bd04d8c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1545894f9035ca585bd4",
+    "url": "/static/css/main.140df8a8.chunk.css"
+  },
+  {
+    "revision": "0d1910dc9e00b8f74175",
+    "url": "/static/js/2.b94a691c.chunk.js"
+  },
+  {
+    "revision": "1545894f9035ca585bd4",
+    "url": "/static/js/main.b152d28a.chunk.js"
+  },
+  {
+    "revision": "579b3c4be38dad109b2a",
+    "url": "/static/js/runtime-main.f34e42b0.js"
+  }
+]);
